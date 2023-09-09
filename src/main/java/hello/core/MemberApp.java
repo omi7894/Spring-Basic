98778db28c2433983994b0d59a1bcd130aa09a9b
MinKyung <omi7894@naver.com>
2023-09-09 17:00:41 +0900

    스프링으로 전환

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,17 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appconfig = new AppConfig();
-        MemberService memberService = appconfig.memberService(); //appconfig를 통해 선언
-        //MemberService memberService = new MemberServiceImpl(memberRepository);
+        //AppConfig appconfig = new AppConfig();
+        //MemberService memberService = appconfig.memberService(); //appconfig를 통해 선언
+
+        //스프링으로 전환하기
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L,"memberA", Grade.VIP);
         memberService.join(member);
