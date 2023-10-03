be5919884165a7b9785d8459225b70f10e87c507
MinKyung <omi7894@naver.com>
2023-10-03 20:39:59 +0900

    컴포넌트 스캔

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     //private DiscountPolicy discountPolicy;
@@ -15,6 +18,7 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { //인터페이스에만 접근
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
